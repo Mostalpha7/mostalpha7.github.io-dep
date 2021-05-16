064a3c04d0c52546e4a1a917e50359a8cd02d4b9
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2021-05-16 21:13:04 +0100

    update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -27,16 +27,22 @@ const Contact = props => {
             <Topbar menuOpen={props.menuOpen} setMenuOpen={props.setMenuOpen} />
             <div className='contact'>
               <div className='wrapper'>
-                <h1>Reach Out</h1>
-                <div className='mt-5'></div>
-                <p>
-                  Interested in working together <br /> or just wanna say "Hi" ?
-                  <br />
-                  Feel free to contact me on project(s) and colaborations.
-                </p>
-                <div className='mt-5'></div>
-                <div className='mt-5'></div>
-                <a href='mailto:mostalpha7@gmail.com'>mostalpha7@gmail.com</a>
+                <div className='makeDown'>
+                  <div></div>
+                  <div>
+                    <h1>Reach Out</h1>
+                    <div className='mt-5'></div>
+                    <div className='mt-5'></div>
+                    <p>Got a project ?</p>
+                    <p>Interested in working together?</p>
+                    <p>Or just wanna say "Hi" ?</p>
+                    <div className='mt-5'></div>
+                    <div className='mt-5'></div>
+                    <a href='mailto:mostalpha7@gmail.com'>
+                      mostalpha7@gmail.com
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </motion.div>
