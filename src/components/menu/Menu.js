ae8d403ff442c71a06b641c1449bad2a795ce91e
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2021-05-16 20:41:24 +0100

    sidedrawer conpleted for v1

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,48 +1,111 @@
 import './menu.scss'
 import { Link } from 'react-router-dom'
-
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import HomeWorkIcon from '@material-ui/icons/HomeWork'
+import PhonelinkRingIcon from '@material-ui/icons/PhonelinkRing'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 const Menu = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className={'menu ' + (menuOpen && 'active')}>
-      <div className='hambuggerContainer'>
-        <div></div>
-        <div
-          className='hambugger'
-          onClick={() => {
-            menuOpen ? setMenuOpen(false) : setMenuOpen(true)
-          }}
-        >
-          <span className='line1'></span>
-          <span className='line2'></span>
-          <span className='line3'></span>
+    <div className={'menu ' + (menuOpen && 'active22')}>
+      <div
+        className={'left ' + (menuOpen && 'active')}
+        onClick={() => {
+          menuOpen ? setMenuOpen(false) : setMenuOpen(true)
+        }}
+      ></div>
+      <div className={'right ' + (menuOpen && 'active')}>
+        <div className='hambuggerContainer'>
+          <div></div>
+          <div
+            className='hambugger'
+            onClick={() => {
+              menuOpen ? setMenuOpen(false) : setMenuOpen(true)
+            }}
+          >
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
+        <div className='menuContainer'>
+          <div>
+            <ul>
+              <li>
+                <a onClick={() => setMenuOpen(false)}>
+                  <Link to='/'>Home</Link>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => setMenuOpen(false)}>
+                  <Link to='/about'>About me</Link>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => setMenuOpen(false)}>
+                  <Link to='/portfolio'>My works</Link>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => setMenuOpen(false)}>
+                  <Link to='/contact'>React Out</Link>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='d-sm-none'>
+            <div>
+              <h2>Get in touch</h2>
+              <div className='mt-5'></div>
+              <div className='mt-5'>
+                <a href='mailto:mostalpha7@gmail.com'>
+                  <MailOutlineIcon className='icon' />
+                  <span className='ml-3'>mostalpha7@gmail.com</span>
+                </a>
+              </div>
+              <div className='mt-5'>
+                <a href='tel:+2348149607707'>
+                  <PhonelinkRingIcon className='icon' />
+                  <span className='ml-3'>+234 814 960 7707</span>
+                </a>
+              </div>
+              <div className='mt-5'>
+                <a>
+                  <HomeWorkIcon className='icon' />
+                  <span className='ml-3'>
+                    16, Adeyemi Street,
+                    <br /> Lagos Nigeria
+                  </span>
+                </a>
+              </div>
+              <div className='mt-5 wrapperSm'>
+                <div className='mt-5 itemCountSm'>
+                  <a href='https://github.com/mostalpha7/' target='_blanck'>
+                    <GitHubIcon className='icon' />
+                  </a>
+
+                  <a href='https://twitter.com/mostalpha7' target='_blanck'>
+                    <TwitterIcon className='icon' />
+                  </a>
+                  <a
+                    href='https://www.linkedin.com/in/mustapha-abdulmujeeb-32680b195/'
+                    target='_blanck'
+                  >
+                    <LinkedInIcon className='icon' />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className='mt-5'>
+            <div></div>
+          </div>
         </div>
       </div>
-      <div className='menuContainer'>
-        <ul>
-          <li>
-            <a onClick={() => setMenuOpen(false)}>
-              <Link to='/'>Home</Link>
-            </a>
-          </li>
-          <li>
-            <a onClick={() => setMenuOpen(false)}>
-              <Link to='/about'>About</Link>
-            </a>
-          </li>
-          <li>
-            <a onClick={() => setMenuOpen(false)}>
-              <Link to='/portfolio'>Portfolio</Link>
-            </a>
-          </li>
-          <li>
-            <a onClick={() => setMenuOpen(false)}>
-              <Link to='/contact'>React Out</Link>
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div></div>
     </div>
   )
 }
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,140 +1,194 @@
-import { useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
-import Loader from '../../components/loader/Loader'
-import Topbar from '../../components/topbar/Topbar'
-import './about.scss'
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
+import Topbar from "../../components/topbar/Topbar";
+import "./about.scss";
 
-const About = props => {
-  const [loader, setLoader] = useState(false)
+const About = (props) => {
+    const [loader, setLoader] = useState(false);
 
-  useEffect(() => {
-    setLoader(true)
+    useEffect(() => {
+        setLoader(true);
 
-    setTimeout(() => {
-      setLoader(false)
-    }, 1000)
-  }, [])
+        setTimeout(() => {
+            setLoader(false);
+        }, 1000);
+    }, []);
 
-  return (
-    <>
-      {loader ? (
-        <Loader />
-      ) : (
-        <>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.65 }}
-          >
-            <Topbar menuOpen={props.menuOpen} setMenuOpen={props.setMenuOpen} />
-            <div className='about'>
-              <div className='wrapper'>
-                <h1 className='mt-5 first'>
-                  <span className='paintYellow'>Hi, </span>I'm Mustapha A.
-                  <br />
-                  Abiodun <span className='paintYellow'>a </span> Website
-                  <span className='paintYellow'> Dev</span> & <br /> Software
-                  Engineer from <span className='paintYellow'> Earth.</span>
-                </h1>
-                <p className='mt-10'>
-                  My purpose is to use my skill, creativity and enthusiasm to
-                  support and inspire others to freely express their talents in
-                  a loving and harmonious way. I'm very passionate about
-                  contributing to the productivity of a value driven
-                  organization.
-                </p>
-                <div>
-                  <div className='languages'>
-                    <h2 className='mt-5'>Main Languages</h2>
-                    <div className='mainLanguages'>
-                      <div className='imgContainer'>
-                        <img
-                          src='assets/languages/main/javascript.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/main/dart.png' alt='' />
-                      </div>
-                    </div>
-                    <h2 className='mt-5'>Frontend Lang</h2>
-                    <div className='mainLanguages'>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/frontend/html5.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/frontend/css3.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/frontend/react.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/frontend/vue.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/frontend/next.png' alt='' />
-                      </div>
-                    </div>
+    return ( <
+        > {
+            loader ? ( <
+                Loader / >
+            ) : ( <
+                >
+                <
+                motion.div initial = {
+                    { opacity: 0 } }
+                animate = {
+                    { opacity: 1 } }
+                transition = {
+                    { duration: 0.65 } } >
+                <
+                Topbar menuOpen = { props.menuOpen }
+                setMenuOpen = { props.setMenuOpen }
+                /> <
+                div className = "about" >
+                <
+                div className = "wrapper" >
+                <
+                h1 className = "mt-5 first" >
+                <
+                span className = "paintYellow" > Hi, < /span>I'm Mustapha A. <
+                br / >
+                Abiodun < span className = "paintYellow" > a < /span> Website <
+                span className = "paintYellow" > Dev < /span> & <br / > Software Engineer from < span className = "paintYellow" > Earth. < /span> <
+                /h1> <
+                p className = "mt-10" >
+                I am a young and purposeful individual willing to improve and also invest my skills, creativity, and enthusiasm to support and inspire others to freely express their talents lovingly and harmoniously. <
+                /p> <
+                div >
+                <
+                div className = "languages" >
+                <
+                h2 className = "mt-5" > Main Languages < /h2> <
+                div className = "mainLanguages" >
+                <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/main/javascript.png"
+                alt = "" /
+                >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/main/dart.png"
+                alt = "" / >
+                <
+                /div> <
+                /div> <
+                h2 className = "mt-5" > Frontend Lang < /h2> <
+                div className = "mainLanguages" >
+                <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/frontend/html5.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/frontend/css3.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/frontend/react.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/frontend/vue.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/frontend/next.png"
+                alt = "" / >
+                <
+                /div> <
+                /div>
 
-                    <h2 className='mt-5'>Backend Lang & Database</h2>
-                    <div className='mainLanguages'>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/backend/nodejs.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img
-                          src='assets/languages/backend/express.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/backend/mongo.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img
-                          src='assets/languages/backend/postgres.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/backend/mysql.png' alt='' />
-                      </div>
-                    </div>
+                <
+                h2 className = "mt-5" > Backend Lang & Database < /h2> <
+                div className = "mainLanguages" >
+                <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/backend/nodejs.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/backend/express.png"
+                alt = "" /
+                >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/backend/mongo.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/backend/postgres.png"
+                alt = "" /
+                >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/backend/mysql.png"
+                alt = "" / >
+                <
+                /div> <
+                /div>
 
-                    <h2 className='mt-5'>Mobile App Lang</h2>
-                    <div className='mainLanguages'>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/mobile/flutter.png' alt='' />
-                      </div>
-                    </div>
-                    <h2 className='mt-5'>Other Skills</h2>
-                    <div className='mainLanguages'>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/others/github.png' alt='' />
-                      </div>
-                      <div className='imgContainer'>
-                        <img
-                          src='assets/languages/others/wordpress.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='imgContainer'>
-                        <img src='assets/languages/others/corel.png' alt='' />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <Link to='/contact'>
-                  <span className='contactMeBtn'>Reach Out</span>
-                </Link>
-              </div>
-            </div>
-          </motion.div>
-        </>
-      )}
-    </>
-  )
-}
+                <
+                h2 className = "mt-5" > Mobile App Lang < /h2> <
+                div className = "mainLanguages" >
+                <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/mobile/flutter.png"
+                alt = "" / >
+                <
+                /div> <
+                /div> <
+                h2 className = "mt-5" > Other Skills < /h2> <
+                div className = "mainLanguages" >
+                <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/others/github.png"
+                alt = "" / >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/others/wordpress.png"
+                alt = "" /
+                >
+                <
+                /div> <
+                div className = "imgContainer" >
+                <
+                img src = "assets/languages/others/corel.png"
+                alt = "" / >
+                <
+                /div> <
+                /div> <
+                /div> <
+                /div> <
+                Link to = "/contact" >
+                <
+                span className = "contactMeBtn" > Reach Out < /span> <
+                /Link> <
+                /div> <
+                /div> <
+                /motion.div> <
+                />
+            )
+        } <
+        />
+    );
+};
 
-export default About
+export default About;
